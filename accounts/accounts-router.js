73baea07f5d86c14a39b2881b06c7f386eb8c9c0
hh <hh@hhmbp15.local>
2020-04-30 23:26:29 +0100

    mvp complete

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -4,6 +4,7 @@ const db = require("../data/dbConfig.js")
 
 const router = express.Router()
 
+// GET
 router.get("/", async (req, res, next) => {
 	try {
 		// SELECT * FROM "accounts"
@@ -14,6 +15,7 @@ router.get("/", async (req, res, next) => {
 	}
 })
 
+// POST
 router.post("/", async (req, res, next) => {
 	try {
 		const payload = {
@@ -32,7 +34,30 @@ router.post("/", async (req, res, next) => {
 })
 
 // PUT
+router.put("/:id", async (req, res, next) => {
+	try {
+		
+		const payload = {
+			name: req.body.name,
+			budget: req.body.budget
+		}
+
+		// translates to `UPDATE "accounts" SET name="?" budget="?" WHERE id=req.params.id`
+		await db("accounts").where("id", req.params.id).update(payload)
+		res.status(204).json(updatedAcc)
+
+	} catch (err) {
+		next(err)
+	}
+})
 
 // DELETE
+router.delete("/:id", async (req, res, next) => {
+
+	// translates to `DELETE FROM "accounts' WHERE id=req.params.id`
+	await db("accounts").where("id", req.params.id).del();
+	const deletedAcc = await db("accounts").where("id", req.params.id)
+	res.json(deletedAcc)
+})
 
 module.exports = router
